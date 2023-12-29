ee235ddc0f67f9d6e7e99d159033d64f56d14ed8
Joe Barnett <joebarnett876@gmail.com>
2023-12-29 15:33:20 +0000

    sel pub wip

--- src/components/SelectedPublications.tsx
+++ src/components/SelectedPublications.tsx
@@ -9,9 +9,7 @@ import Reference from "./Reference";
 //   string,
 //   {
 //     title: string;
-//     description?: string;
-//     bullet?: boolean;
-//     sections: Array<{ title?: string; content: string }>;
+//     sections: Array<{ content: string }>;
 //   }
 // >;
 
@@ -120,3 +118,50 @@ export default function SelectedPublications() {
     </Section>
   );
 }
+
+// import React from "react";
+// import Section from "./Section";
+// import Markdown from "react-markdown";
+
+// const selectedPublications = import.meta.glob(
+//   "../../site/content/selected-publications/*.json",
+//   { eager: true }
+// ) as Record<
+//   string,
+//   {
+//     title: string;
+//     sections: Array<{ content: string; doi?: string }>;
+//   }
+// >;
+
+// export default function SelectedPublications() {
+//   return (
+//     <Section title="Selected Publications">
+//       {Object.values(selectedPublications).map(({ title, sections }, idx) => {
+//         return (
+//           <React.Fragment key={idx}>
+//             <h3>{title}</h3>
+//             <ul>
+//               {sections.map(({ content, doi }) => {
+//                 return (
+//                   <li>
+//                     <p>
+//                       <Markdown>{content}</Markdown>{" "}
+//                       {doi && (
+//                         <span>
+//                           <a href={doi} target="_blank">
+//                             [doi]
+//                           </a>
+//                         </span>
+//                       )}
+//                     </p>
+//                   </li>
+//                 );
+//               })}
+//             </ul>
+//           </React.Fragment>
+//         );
+//       })}
+//     </Section>
+//   );
+// }
