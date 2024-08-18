4d954d64787b8ce79a8d192a366e85e1cd1ac0c6
Joe Barnett <joebarnett876@gmail.com>
2024-08-18 18:29:55 +0100

    wip

--- src/components/SelectedPublications.tsx
+++ src/components/SelectedPublications.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Section from "./Section";
 import Markdown from "react-markdown";
-import selectedPublications from "../../site/content/selected-publications.md?raw";
+
+import selectedPublications from "../../site/content/selected-publications.json";
 
 export default function SelectedPublications() {
   return (
@@ -13,40 +14,8 @@ export default function SelectedPublications() {
           },
         }}
       >
-        {selectedPublications}
+        {selectedPublications.content}
       </Markdown>
-      {/* {Object.values(selectedPublications).map(({ title, content }, idx) => {
-        return (
-          <React.Fragment key={idx}>
-            <h3>{title}</h3>
-            <ul>
-              {sections.map(({ content, doi }, idx) => {
-                return (
-                  <li key={idx} style={{ paddingBottom: "1rem" }}>
-                    <Markdown
-                      components={{
-                        p(props) {
-                          return <p style={{ display: "inline" }} {...props} />;
-                        },
-                      }}
-                    >
-                      {content}
-                    </Markdown>
-                    {doi && (
-                      <span>
-                        {" "}
-                        <a href={doi} target="_blank">
-                          [doi]
-                        </a>
-                      </span>
-                    )}
-                  </li>
-                );
-              })}
-            </ul>
-          </React.Fragment>
-        );
-      })} */}
     </Section>
   );
 }
